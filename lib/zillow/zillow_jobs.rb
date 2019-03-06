f3df42f16cbf8664b30ad0ef5553689bd8c2ffec
Hebron George <hebrontgeorge@gmail.com>
2019-03-06 03:59:58 -0600

    method signatures to pull some property's details from the zillow api

---
+++ lib/zillow/zillow_jobs.rb
@@ -0,0 +1,24 @@
+require './lib/zillow/api'
+
+module ZillowJobs
+  def self.run_daily_property_updates(zpids = [])
+    zpids.each do |zpid|
+      api.pull_property_details(zpid)
+    end
+  end
+
+  private
+
+  def self.api
+    @api ||= Zillow::API.new
+  end
+
+end
+
+def self.get_property_zpids
+  House.all_property_zpids
+end
+
+
+zillow_property_ids = get_property_zpids
+ZillowJobs.run_daily_property_updates(zillow_property_ids)
