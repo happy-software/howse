8225b08651cfc81a9e54a40fc84359263096de69
Hebron George <hebrontgeorge@gmail.com>
2020-01-03 21:30:10 -0600

    Add route to look up zpid

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   resources :houses
   resources :home_valuations
+
+  namespace :api do
+    namespace :v1 do
+      post 'zillow_search', action: 'lookup_address', controller: 'zillow_search'
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
