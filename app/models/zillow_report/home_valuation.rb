88d5d7bfcf814313ca4f442f8039e4c3e905c70d
Hebron George <hebrontgeorge@gmail.com>
2020-01-18 16:42:22 -0600

    Use with_indifferent_access

--- app/models/zillow_report/home_valuation.rb
+++ app/models/zillow_report/home_valuation.rb
@@ -4,7 +4,7 @@ module ZillowReport
     belongs_to :house, foreign_key: :zpid, primary_key: :zpid
 
     def self.add_new_response(response)
-      response               = response.deep_symbolize_keys!
+      response               = response.with_indifferent_access
       valuation              = ZillowReport::HomeValuation.new
       valuation.raw_response = response.delete(:xml).to_s
       valuation.details      = response
